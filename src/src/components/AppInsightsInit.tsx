3fcf3c9fcf147b16d885feac012d985c0cc599fd
Kieron Lanning <kieronlanning@users.noreply.github.com>
2025-05-26 23:31:53 +0100

    chore: removed all warnings

--- src/src/components/AppInsightsInit.tsx
+++ src/src/components/AppInsightsInit.tsx
@@ -5,6 +5,9 @@ export default function AppInsightsInit() {
 	useEffect(() => {
 		// Already initialized via import side effect,
 		// but we can setup other things here if needed
+		if (appInsights.config?.enableDebug === true) {
+			console.log('App Insights initialized with config:', appInsights.config);
+		}
 	}, []);
 	return null;
 }
