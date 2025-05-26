f6b24729f85edaf8e8935f5657fc6040a2320c4d
Kieron Lanning <kieronlanning@users.noreply.github.com>
2025-05-26 14:25:28 +0100

    fix: removed most warnings

--- src/src/components/AppInsightsInit.tsx
+++ src/src/components/AppInsightsInit.tsx
@@ -3,8 +3,9 @@ import { appInsights } from '../scripts/appInsights';
 
 export default function AppInsightsInit() {
 	useEffect(() => {
-		// Already initialized via import side effect,
-		// but we can setup other things here if needed
+		if (appInsights.config.enableDebug === true) {
+			console.log('App Insights debugging enabled');
+		}
 	}, []);
 	return null;
 }
